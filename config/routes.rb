a7078584df8dbfc1d4e0b5b4ffbcdb5f63615e21
Rayan <rayan_84@live.com>
2021-02-06 13:34:17 +0100

    Add update to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  resources :users, only: [:new, :create, :edit, :show, :index]
+  resources :users, only: [:new, :create, :edit, :show, :index, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
